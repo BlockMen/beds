4824ea56ccc8869936e1722d3e7a50498cde6962
BlockMen <nmuelll@web.de>
2014-08-07 16:42:12 +0200

    Clean up, fix rare position bug

--- init.lua
+++ init.lua
@@ -75,16 +75,14 @@ local function lay_down(player, pos, bed_pos, state)
 		player_in_bed = player_in_bed + 1
 
 		-- physics, eye_offset, etc
-		player:set_eye_offset({x=0,y=-13,z=0}, {x=0,y=0,z=0})--8
+		player:set_eye_offset({x=0,y=-13,z=0}, {x=0,y=0,z=0})
 		local yaw, param2 = get_look_yaw(bed_pos)
 		player:set_look_yaw(yaw)
 		local dir = minetest.facedir_to_dir(param2)
 		local p = {x=bed_pos.x+dir.x/2,y=bed_pos.y,z=bed_pos.z+dir.z/2}
-		--p.y = p.y + 0.2
-		player:setpos(p)
+		player:set_physics_override(0, 0, 0)
 		player:setpos(p)
 		default.player_attached[name] = true
-		player:set_physics_override(0, 0, 0)
 		hud_flags.wielditem = false
 		default.player_set_animation(player, "lay" , 0)
 	end
